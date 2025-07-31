edc99579955a99754a69bb68e824b31c45c151b3
juanySC <48737486+juanySC@users.noreply.github.com>
2025-07-31 16:59:00 -0600

    verificacion si cumple, que tipo de triangulo es

---
+++ tareas/tarea2/TrianguloClasificacion.java
@@ -0,0 +1,67 @@
+/*
+* Clasificación de triángulos: lea tres longitudes, valide que
+formen un triángulo y clasifíquelo como equilátero, isósceles o
+escaleno. Además, indique si es rectángulo.*/
+package org.example;
+import java.util.Scanner;
+
+public class TrianguloClasificacion {
+
+    public static boolean esTriangulo(double lado1, double lado2, double lado3){
+        /*La suma de dos lados debe ser mayor que el tercero.
+          Ejemplo: 3, 4, 5 sí forman un triángulo.
+          Ejemplo: 2, 2, 5 no (porque 2 + 2 no es mayor que 5).*/
+        return ((lado1 + lado2 > lado3) && (lado2 + lado3 > lado1) && (lado1 + lado3 > lado2));
+    }
+
+    public static String tipoTriangulo(double lado1, double lado2, double lado3){
+        if((lado1 == lado2) && (lado1 == lado3)){
+            return "---triangulo equilatero---";
+        } else if (((lado1 == lado2) && (lado1 != lado3)) || ((lado1 == lado3) && (lado1 != lado2)) || ((lado2 == lado3) && (lado2 != lado1))) {
+            return "---triangulo isosceles---";
+        } else {
+            return "---Triangulo escaleno---";
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String continuarUsuario;
+        double ladoA , ladoB , ladoC;
+
+        do{
+            System.out.println("\nEste programa permite verificar que tipo de triangulo ingreso");
+            System.out.println("\nIngrese el primer lado del triangulo entero/decimal: ");
+            ladoA = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("Ingrese el segundo lado del triangulo entero/decimal: ");
+            ladoB = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("Ingrese el tercer lado del triagulo entero/decimal:");
+            ladoC = sc.nextDouble();
+            sc.nextLine();
+
+            //calsificando si es o no un triangulo
+            boolean verificacion = esTriangulo(ladoA, ladoB, ladoC);
+            String tiposTri = tipoTriangulo(ladoA, ladoB, ladoC);
+
+            if(verificacion){
+                System.out.println("\nLos lados que ingreso cumplen con un triagunlo");
+                System.out.println("El tipo de triangulo es: "+tiposTri);
+            }else{
+                System.out.println("\nNo cumple con un triangulo");
+            }
+
+            //clasifico el tipo del triagunlo
+
+            //le pregunto al usuario si desea continuar
+            System.out.println("\n¿Desea ejecutar el programa nuevamente? s/n ");
+            continuarUsuario = sc.nextLine();
+        }while (continuarUsuario.equalsIgnoreCase("s"));
+
+        sc.close();
+        System.out.println("Gracias por usar el programa");
+    }
+}
