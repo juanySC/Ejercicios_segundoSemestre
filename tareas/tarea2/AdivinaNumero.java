ebe6d38d5c5f9e2a6caa8461a6f83eeded72530a
juanySC <48737486+juanySC@users.noreply.github.com>
2025-08-01 21:49:44 -0600

    todo funciona

---
+++ tareas/tarea2/AdivinaNumero.java
@@ -0,0 +1,52 @@
+/*Adivina el número: genere aleatoriamente un entero 1-100. El
+usuario intenta adivinar; después de cada intento indique “más
+alto” o “más bajo”. Limite a 5 intentos.*/
+package org.example;
+import java.util.Scanner;
+
+public class AdivinaNumero {
+
+    /**verificarNumero ayuda a comprar el numero que ingresa el usuario con el numero
+     * generado de manera aleatoria**/
+    public static void verificarNumero(){
+        Scanner sc = new Scanner(System.in);
+        int numeroSecreto = (int)(Math.random() * 100) + 1;
+        int intento = 5, numero;
+
+        do{
+            System.out.println("Ingresa el número: ");
+            numero = sc.nextInt();
+            sc.nextLine();
+
+            //comparo
+            if(numero == numeroSecreto){
+                System.out.println("Acertaste! el numero era :" + numeroSecreto);
+            } else if (numero > numeroSecreto) {
+                System.out.println("Pista: el número tiene que ser menor");
+            } else if(numero < numeroSecreto){
+                System.out.println("Pista: el número tiene que ser mayor");
+            } else {
+                System.out.println("Terminaste tus intentos! suerte para la proxima");
+            }
+
+        }while (numeroSecreto != intento);
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String continuarUsuario;
+
+        System.out.println("Adivina el número y te dare pistas");
+        //para repetir el programa
+        do{
+            //llamo a la funcion
+            verificarNumero();
+
+            //le pregunto al usuario si desea seguir o terminar el programa
+            System.out.println("¿Deseas repetir el programa? s/n ");
+            continuarUsuario = sc.nextLine();
+        }while (continuarUsuario.equalsIgnoreCase("s"));
+
+    }
+}
