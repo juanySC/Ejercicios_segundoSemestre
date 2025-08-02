328c851fbfa423a4e8bb215aa0f8d1d6a7aca6b2
juanySC <48737486+juanySC@users.noreply.github.com>
2025-08-01 19:31:12 -0600

    metodos funcionales

--- tareas/tarea2/GestorInventario.java
+++ tareas/tarea2/GestorInventario.java
@@ -7,18 +7,20 @@ import com.sun.security.jgss.GSSUtil;
 import  java.util.Scanner;
 
 public class GestorInventario {
+    static int mercancia = 0;
 
-    /**menu ayuda a mostrar al usuariolas opciones disponibles que puede hacer en el inventario **/
+
+    /**menu ayuda a mostrar al usuario las opciones disponibles que puede hacer en el inventario **/
     public static void menu(){
         //opciones que tendran para seleccionar
         Scanner sc = new Scanner(System.in);
         String continuar;
         int opcion;
 
-        System.out.println("Este programa es un gestor basico de inventario");
+        System.out.println("\nEste programa es un gestor basico de inventario");
 
         do {
-            System.out.println("Seleccione la opción que desee");
+            System.out.println("\nSeleccione la opción que desee");
             System.out.println("1. Agregar existencias");
             System.out.println("2. Retirar existencias");
             System.out.println("3. Consultar stock");
@@ -29,17 +31,15 @@ public class GestorInventario {
             //opciones
             switch (opcion){
                 case 1:
-                    int valorCantidad;
-                    System.out.println("Ingrese la cantidad que desee agregar");
-                    valorCantidad = sc.nextInt();
+                    System.out.println("\nIngrese la cantidad que desee agregar");
+                    int valorCantidad = sc.nextInt();
                     sc.nextLine();
 
                     agregarStock(valorCantidad);
                     break;
                 case 2:
-                    int valorRetirado;
-                    System.out.println("Ingrese la cantidad que desee agregar");
-                    valorRetirado= sc.nextInt();
+                    System.out.println("\nIngrese la cantidad que desee retirar");
+                    int valorRetirado= sc.nextInt();
                     sc.nextLine();
 
                     retirarExistencia(valorRetirado);
@@ -51,11 +51,11 @@ public class GestorInventario {
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Opción no disponible, intente de nuevo");
+                    System.out.println("\nOpción no disponible, intente de nuevo");
             }
 
             //le pregunto al usuario si quiere repetir
-            System.out.println("¿Deseas repetir el programa? s/n ");
+            System.out.println("\n¿Deseas repetir el programa? s/n ");
             continuar = sc.nextLine();
         }while (continuar.equalsIgnoreCase("s"));
         sc.close();
@@ -63,20 +63,35 @@ public class GestorInventario {
     }
 
     /**agregarStock nos ayuda a agregar nuevos elementos
-     * @param cantidad  dato numerico que indica cuantos elementos se agregan a la lista**/
-    public static int agregarStock(int cantidad){
-        return cantidad;
+     * @param cantidadAgregar  dato numerico que indica cuantos elementos se agregan a la lista**/
+    public static int agregarStock(int cantidadAgregar){
+        //comparo si los numeros no son negativos
+        if (cantidadAgregar>0){
+            mercancia += cantidadAgregar;
+            System.out.println("Se agregan "+cantidadAgregar+" de unidades");
+        } else {
+            System.out.println("Cantidad invalida, vuelva a intentarlo");
+        }
+        return mercancia;
     }
 
     /**retirarExistencia nos ayuda a eliminar ciertos elementos de la lista
-     * @param cantidad  dato numerico que indica cuantos elementos se eliminan**/
-    public static int retirarExistencia(int cantidad){
-        return cantidad;
+     * @param cantidadRetirar  dato numerico que indica cuantos elementos se eliminan**/
+    public static int retirarExistencia(int cantidadRetirar){
+        //que no sobrepase a mi numero de existencia
+        if ((cantidadRetirar>0) && (cantidadRetirar <=mercancia)){
+            mercancia -= cantidadRetirar;
+            System.out.println("Se retiran "+cantidadRetirar+" de unidades");
+        }else {
+            System.out.println("Cantidad invalida, no hay suficiente mercaderia");
+        }
+        return mercancia;
     }
 
     /**mostrarStock solo imprime lo que esta en existencias actuales**/
     public static void mostrarStock(){
-
+        //solo imprimo
+        System.out.println("La mercancia actual es: "+mercancia);
     }
 
     public static void main(String[] args) {
