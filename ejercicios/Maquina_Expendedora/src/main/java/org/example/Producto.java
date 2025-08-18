97dd901db4f90555ef289ce6dae589baec2b39ca
juanySC <48737486+juanySC@users.noreply.github.com>
2025-08-18 11:24:58 -0600

    Add files via upload

---
+++ ejercicios/Maquina_Expendedora/src/main/java/org/example/Producto.java
@@ -0,0 +1,78 @@
+package org.example;
+
+public class Producto {
+
+        //atributos
+        public String codigo;
+        public String nombre;
+        public double precio;
+        public int stock;
+
+
+    //creando mi constructor
+        Producto(String codigo, String nombre, double precio, int stock){
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.stock = stock;
+        }
+
+        //getters and setters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+        //metodos
+        public static boolean hayStock() {
+        return false;
+    }
+
+        public static int venderUna() {
+           if (this.stock){
+               stock --;
+           }
+    }
+
+        public static int reponer(int cantidad){
+            this.stock += 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
+
+    }
