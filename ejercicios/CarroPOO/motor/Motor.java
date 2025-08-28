8d2ef6d05db9048a7da54510d2d5dfe4c814f356
juanySC <48737486+juanySC@users.noreply.github.com>
2025-08-28 17:32:07 -0600

    ejercicio de motor-tanque-carro
    por ahora solo tengo la estructura inicial de clase Motor, en una carpeta aparte

---
+++ ejercicios/CarroPOO/motor/Motor.java
@@ -0,0 +1,42 @@
+/*1.Clase Motor
+Atributos: eficienciaKmL (double, km por litro),encendido (boolean).
+
+Métodos: encender(), apagar(), isEncendido(),getEficienciaKmL().
+*/
+package motor;
+
+public class Motor {
+    //atributos
+    private double eficienciaKmL;
+    private boolean encendido;
+
+    //constructor
+    //solo coloco eficiencia porque se supone que el motor siempre esta apagado al inicio
+    public Motor(double eficienciaKmL){ 
+        this.eficienciaKmL = eficienciaKmL;
+        this.encendido = false;
+    }
+
+    //metodos
+    /**encender me ayuda a saber si el motor esta encendido y si es así
+     * entonces me devuelve una confirmación**/
+    public void encender(){
+        this.encendido = true;
+    }
+
+    /**apagar si el motor esta encendido utilizo este metodo para cambiarlo
+     * de estado y apagarlo**/
+    public void apagar(){
+        this.encendido = false;
+    }
+
+    //getters y setters
+    public boolean isEncendido(){
+        return this.encendido;
+    }
+
+    public double getEficienciaKmL(){
+        return this.eficienciaKmL;
+    }
+    //toString
+}
