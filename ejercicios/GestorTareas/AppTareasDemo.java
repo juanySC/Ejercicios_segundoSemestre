a7a8865f993160f372f368fe693a8a46e9abd1a4
juanySC <48737486+juanySC@users.noreply.github.com>
2025-09-07 16:03:07 -0600

    completo y funcional

--- ejercicios/GestorTareas/AppTareasDemo.java
+++ ejercicios/GestorTareas/AppTareasDemo.java
@@ -1,9 +1,120 @@
+import java.util.Scanner;
+
 import javax.swing.text.TabSet;
 
 import gestor.GestorTareas;
 import tarea.Tarea;
 
 public class AppTareasDemo{
+
+    
+
+     /**menu muestra las acciones que podemos realizar
+      * desde las clases gestorTareas y tarea**/
+    public static void menu(){
+        Scanner sc = new Scanner(System.in);
+        //instancio mis clases para menu
+        Tarea tareaMenu = new Tarea(null, null);
+        GestorTareas gestorMenu = new GestorTareas();
+        while (true) {
+            System.out.println("\nMenú de opciones eliga el que desee: ");
+        System.out.println("1. Crear tarea");
+        System.out.println("2. Listar tareas");
+        System.out.println("3. Listar tareas pendientes");
+        System.out.println("4. Listar tareas completadas");
+        System.out.println("5. Marcar como completado por id");
+        System.out.println("6. Alterar estaado id");
+        System.out.println("7. Eliminar estado por id");
+        System.out.println("8. Buscar tarea por id");
+        System.out.println("9. Salir");
+        int opcion = sc.nextInt();
+        sc.nextLine();
+
+        switch (opcion) {
+            case 1:
+            String continuar;
+                do {
+                    System.out.println("Ingrese el titutlo de la tarea");
+                    String titulo = sc.nextLine();
+                    System.out.println("Ingrese la descripcion de la tarea");
+                    String descripcion = sc.nextLine();
+                
+                    gestorMenu.crearTarea(titulo, descripcion);
+                    //le pregunto al usuario si desea añadir otra tarea o no
+                    System.out.println("¿deseas añadir otra tarea? s/n");
+                    continuar = sc.nextLine();
+                } while (continuar.equalsIgnoreCase("s"));
+
+                
+                break;
+            case 2:
+                //muestra la lista de tareas
+                gestorMenu.listarTarea();
+                break;
+            case 3:
+                gestorMenu.listarPendiente();
+                break;
+            case 4:
+                gestorMenu.listarCompletadas();
+                break;
+            case 5:
+                //ingresa por id
+                System.out.println("Ingrese el id para poder marcar como completa");
+                String id = sc.nextLine();
+
+                if (gestorMenu.completarTarea(id)) {
+                    System.out.println("La tarea con id: "+id + "ha sido completada");
+                } else {
+                    System.out.println("No se encontro ninguna coincidencia con el id "+id);
+                }
+                break;
+            case 6:
+                //alterar por id
+                System.out.println("Ingrese el id para alternar su estado: ");
+                String alternar = sc.nextLine();
+
+                if (gestorMenu.alternarEstado(alternar)) {
+                    System.out.println("La tarea con id" + alternar + "se ha hecho con exito");
+                } else{
+                   System.out.println("No se encontro ninguna coincidencia con el id "+alternar);
+                }
+                break;
+            case 7:
+             //eliminar por id
+                System.out.println("Ingrese el id para eliminar la tarea ");
+                String eliminar = sc.nextLine();
+
+                if (gestorMenu.eliminarTarea(eliminar)) {
+                    System.out.println("La tarea con id" + eliminar + "se ha hecho con exito");
+                } else{
+                   System.out.println("No se encontro ninguna coincidencia con el id "+eliminar);
+                }
+                break;
+            case 8:
+                //llamo a mi metodo buscar
+                System.out.println("Ingrese el id para buscarlo en la lista");
+                String buscar = sc.nextLine();
+                //creo mi nuevo objeto
+               Tarea tareaBuscar = gestorMenu.buscarTarea(buscar);
+                if (tareaBuscar!= null) {
+                    //le devuelvo la descripcion
+                    System.out.println("la tarea fue encontrada" +tareaBuscar.toString());
+                }else{
+                    System.out.println("La tarea no fue encontrada, no existe" + tareaBuscar);
+                }
+                break;
+            case 9:
+                System.out.println("Gracias por usar el programa!");
+                sc.close();
+                System.exit(0);
+                break;                                        
+            default:
+                System.out.println("La opcion que desea no esta disponible");
+                break;
+        }
+        }
+        
+    }
     public static void main(String[] args){
 
         //CLASE TAREA ----------------------------------------------------
@@ -69,5 +180,9 @@ public class AppTareasDemo{
         //muestro las tareas restante
         System.out.println("\nTareas actuales");
         gestorT.listarTarea();
+
+        //MENU-----------------------------------------------------------
+        menu();
+
     }
 }
