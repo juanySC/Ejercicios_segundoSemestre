30e133f6b559e6e403d318342b88bb834f9cc71c
juanySC <48737486+juanySC@users.noreply.github.com>
2025-09-10 08:23:40 -0600

    primeros dos ejercicios resueltos
    sobre fechas, plusDays, minusDays, el formato de las fechas

---
+++ tareas/ejercicioFechas/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+/*. Mostrar la fecha actual
+Crea un programa que imprima la fecha de hoy (LocalDate.now()).
+Agrega el día de la semana (getDayOfWeek()).
+*/package org.example;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Este programa te indica la fecha de hoy y el dia");
+        //declaro mi localDate y el .now me sirve para tomar la fecha actual
+        LocalDate fechaActual = LocalDate.now();
+        System.out.println("\nLa fecha de hoy es: "+ fechaActual);
+        //creo mi variable
+        DayOfWeek diaSemana = fechaActual.getDayOfWeek();
+        System.out.println("\nEl día de hoy es: "+ diaSemana);
+
+
+    }
+}
---
+++ tareas/ejercicioFechas/src/main/java/org/example/SumarRestarDias.java
@@ -0,0 +1,58 @@
+/*2. Sumar y restar días
+Pide una fecha al usuario (ej. 2025-09-10).
+Muestra:
+la fecha 10 días después,
+y la fecha 15 días antes.*/
+package org.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class SumarRestarDias {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String continuar;
+        //el tipo de formato que quiero que reciba la fecha, en este caso el año va primero
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        System.out.println("Este programa suma y resta fechas");
+        do {
+            String fecha;
+
+            System.out.println("Ingresa la primera fecha: (ej. 2025-09-10)");
+            fecha = sc.nextLine();
+
+            try{
+            //convierto mi cadena ingresada a un objeto
+            //le paso los parametros ingresados por el usuario
+            LocalDate conversion = LocalDate.parse(fecha, formato);
+            //lo muestri
+            System.out.println("La fecha uno ha sido ingresada correcta: "+conversion);
+
+            //le sumo a la fecha +10
+                int diasExtras = 10;
+                //plusDays nos siver para sumarle  traves de un int los dias a la fecha
+                LocalDate fechaFutura = conversion.plusDays(diasExtras);
+                System.out.println("La fecha actual dentro de 15 días es: "+ fechaFutura);
+
+                //ahora hago la resta de fechas -15
+                int diasAtras = 15;
+                //minusDays nos sirve para restarle a traves de un int los dias a la fecha
+                LocalDate fechaPasada = conversion.minusDays(diasAtras);
+                System.out.println("La fecha pasada de 15 días es: "+fechaPasada);
+
+
+            }catch (DateTimeParseException e){
+                //indica que existe un problema en pasar una cadena de texto a una fecha, por algo mal ingresado
+                System.out.println("La fecha ingresada no es correcta por el formato");
+            }
+
+
+
+            System.out.println("¿Deseas ingresar otra fecha? s/n");
+            continuar = sc.nextLine();
+        } while (continuar.equalsIgnoreCase("s"));
+    }
+}
